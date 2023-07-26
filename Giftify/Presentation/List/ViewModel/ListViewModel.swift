f67e8a070dca05d7cb24a954c2af43ea9c95c7d7
yummyhawaiianpizzakim <yummyhawaiianpizzakim@gmail.com>
2023-07-26 16:04:29 +0900

    listViewmModel 생성 // settingViewModel 생성

---
+++ Giftify/Presentation/List/ViewModel/ListViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  ListViewModel.swift
+//  Giftify
+//
+//  Created by 김요한 on 2023/07/25.
+//
+
+import Foundation
+
+class ListViewModel {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        let output = Output()
+        
+        return output
+    }
+}
